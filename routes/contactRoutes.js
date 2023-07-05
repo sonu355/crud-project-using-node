49c230850ad5b671d8058b38e0db69838e079cde
harshal <harshalc562@gmail.com>
2023-07-06 01:51:42 +0530

    created routes

---
+++ routes/contactRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.route('/',(req, res) => {
+    res.json({message: "get all contacts"})
+})
+
+module.exports = router;
